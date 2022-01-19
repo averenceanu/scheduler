4e4a60a6ed00d7b45bf2f25e3d0cfd33c4f583cf
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-01-19 18:22:43 -0500

    Empty and Header componenet build

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./styles.scss"
+import Header from "./Header.js"
+import Empty from "./Empty.js"
+
+export default function Appointment (props) {
+  return (
+    <article className="appointment"></article>
+  );
+}
