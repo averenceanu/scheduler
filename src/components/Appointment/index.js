35cfc692b6fdee2e5dd1d424562bbbfba82eeebd
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-01-19 18:46:57 -0500

    completed all part of appointment componenet but form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import "./styles.scss"
 import Header from "./Header.js"
 import Empty from "./Empty.js"
+import Show from "./Show.js"
+import Confirm from "./Confirm.js"
+import Status from "./Status.js"
+import Error from "./Error.js"
 
 export default function Appointment (props) {
   return (
