177f7ab608150b4353c03af4d031367774c780c6
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-01-24 12:09:01 -0500

    build custom hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+  function useVisualMode (initial) {
+  const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial])
+  
+  const transition = function (updateMode, replace = false) {
+    if (replace) {
+      setMode(updateMode)
+    } else {
+      setMode(updateMode)
+      setHistory([...history, updateMode])
+    }
+  }
+
+  const back = function () {
+    history.pop()
+    setHistory([...history])
+    if (history.length >= 1) {
+    setMode(history[history.length - 1])
+    }
+  }
+  return { mode, transition, back } ;
+}
+
+
+
+export default useVisualMode
