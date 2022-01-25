7a893eb6da2ced6c7372baed3690ce60594633e1
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-01-24 20:39:19 -0500

    edit interview function add

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -17,7 +17,8 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
-const CONFIRM = "CONFIRM"
+const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
 
 export default function Appointment (props) {  
   //console.log(props)
@@ -58,6 +59,7 @@ export default function Appointment (props) {
           student={props.interview.student} //props.interview.student
           interviewer={props.interview.interviewer}
           onDelete={() => transition(CONFIRM)} //props.onDelete
+          onEdit={() => transition(EDIT)}
         />
       )}
       {mode === CREATE && (
@@ -78,6 +80,15 @@ export default function Appointment (props) {
           onCancel={() => back()}
           onConfirm={() => cancel()}
           message={"Are you sure you want to delete this?"}
+        />
+      )}
+      {mode === EDIT && (
+        <Form 
+          interviewers={props.interviewers} 
+          onCancel={() => {back()}}
+          onSave={save}
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.id}
         />
       )}
       
